f251d75f0e95d94e83c08e53c8cd46f86815c815
apontes77 <alexandrepontesgyn@gmail.com>
2020-11-25 22:30:02 -0300

    feat: using a template in react app

--- frontend/ecommerce/src/App.js
+++ frontend/ecommerce/src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+    }
+
+    const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open");
+    }
+
+  return ( 
+    <div class="grid-container">
+        <header class="header">
+            <div class="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <a href="index.html"> Shopping do Povo</a>
+            </div>
+            <div class="header-links">
+                <a href="pagamento.html">Pagamento</a>
+            </div>          
+        </header>
+        <aside class="sidebar">
+            <h3>Categorias de Produtos</h3>
+            <button  class="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+                <li>
+                    <a href="index.html">PCs e Notebooks</a>
+                </li>
+                <li>
+                    <a href="index.html">Periféricos</a>
+                </li>
+            </ul>
+        </aside>
+        <main class="main">
+            <div class="content">
+                <ul class="products">
+                    <li>
+                        <div class="product">
+                            <img class="product-image" src="/images/fone.jpg" alt="Fone JBL Quantum 100"/>
+                            <div class="product-name">
+                                <a href="product.html">Headset</a>
+                            </div>
+                            <div class="product-brand">JBL</div>
+                            <div class="product-price">R$ 272.00</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="product">
+                            <img class="product-image" src="/images/fone.jpg" alt="Fone JBL Quantum 100"/>
+                            <div class="product-name">
+                                <a href="product.html">Headset</a>
+                            </div>
+                            <div class="product-brand">JBL</div>
+                            <div class="product-price">R$ 272.00</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="product">
+                            <img class="product-image" src="/images/fone.jpg" alt="Fone JBL Quantum 100"/>
+                            <div class="product-name">
+                                <a href="product.html">Headset</a>
+                            </div>
+                            <div class="product-brand">JBL</div>
+                            <div class="product-price">R$ 272.00</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="product">
+                            <img class="product-image" src="/images/fone.jpg" alt="Fone JBL Quantum 100"/>
+                            <div class="product-name">
+                                <a href="product.html">Headset</a>
+                            </div>
+                            <div class="product-brand">JBL</div>
+                            <div class="product-price">R$ 272.00</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="product">
+                            <img class="product-image" src="/images/fone.jpg" alt="Fone JBL Quantum 100"/>
+                            <div class="product-name">
+                                <a href="product.html">Headset</a>
+                            </div>
+                            <div class="product-brand">JBL</div>
+                            <div class="product-price">R$ 272.00</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
+        </main>
+        <footer class="footer">Todos os direitos reservados</footer>
     </div>
   );
 }
