983ab3b77581701bb854c4da740b02b4f9eac6fb
apontes77 <alexandrepontesgyn@gmail.com>
2020-11-25 21:38:45 -0300

    feat: creating a Products List and a Sidebar

--- frontend/ecommerce/src/index.js
+++ frontend/ecommerce/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Main from './Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+      <Main />
     <App />
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
