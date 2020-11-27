1b91857ebc662a2f541906cdad4ec7ddde9be6fe
LeonardoFreitas1 <leo_paulino2011@hotmail.com>
2020-11-26 23:31:06 -0300

    upload de imagem

--- backend/src/controllers/ProductController.js
+++ backend/src/controllers/ProductController.js
@@ -1,16 +1,29 @@
 const express = require('express')
-
+const multer = require('multer')
+const multerConfig = require('../config/multer')
 const Product = require('../models/Products')
 
 const router = express.Router()
 
-router.post('/registerProduct', async (req, res) => {
+router.post('/registerProduct', multer(multerConfig).single('file'), async (req, res) => {
+    const { name, valueUnit, description, amount } = req.body
+    const { key,  location } = req.file
+
+    console.log(name)
+  
     try {
-        const product = await Product.create(req.body)
+        const product = await Product.create({
+            name: name,
+            valueUnit,
+            description,
+            amount,
+            file_name: key,
+            file_url: location,
+        })
 
         return res.send({ product })
     }catch(err) {
-        return res.status(400).send({ error: 'Registration Failed' })
+        return res.status(400).send({ error: err })
     }
 })
 
